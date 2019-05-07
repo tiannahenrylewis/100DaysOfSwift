419cdea97c14140ec5a962e810254e1cffd4ecbe
Tianna Henry-Lewis <40465736+tiannahenrylewis@users.noreply.github.com>
2019-05-06 22:59:03 -0400

    Day 42
    NameToFaces
    - Set-up collectionView
    - Modified prototype cell to include ImageView and Label

---
+++ NameToFaces/NameToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NameToFaces
+//
+//  Created by Tianna Henry-Lewis on 2019-05-06.
+//  Copyright © 2019 Tianna Henry-Lewis. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
---
+++ NameToFaces/NameToFaces/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  NameToFaces
+//
+//  Created by Tianna Henry-Lewis on 2019-05-06.
+//  Copyright © 2019 Tianna Henry-Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK: - Collection View Data Source
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue a PersonCell.")
+        }
+        return cell
+    }
+
+
+}
+
